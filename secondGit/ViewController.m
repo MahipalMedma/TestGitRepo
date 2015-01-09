63ae7ba43058b6781272a076e7e023b57b5d206b
Medma Infomatix <medma@MedmaMahipal.local>
2015-01-09 11:04:23 +0530

    test

--- secondGit/ViewController.m
+++ secondGit/ViewController.m
@@ -21,7 +21,15 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+
+    int a = 5;
+    int b = 10;
+
+    self.sum = a + b;
+
+    NSLog(@"The result is: %d", self.sum);
+
 }
 
 @end
